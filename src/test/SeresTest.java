e59628a789a6c96acb977eea293e03856984a237
Isa <isarodriguezg10@gmail.com>
2019-05-04 18:46:54 +0200

    test clase Seres

--- src/test/SeresTest.java
+++ src/test/SeresTest.java
@@ -1,15 +1,38 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
-class SeresTest {
+import control.Estado;
+import modelo.poblacion.Seres;
 
+class SeresTest {
+	
+	Estado estado = new Estado(0, 0, 0);
+	Seres ser = new Seres();
+		
 	@Test
 	void testCalculaParteProporcional() {
 		
+		float necesidadVital = ser.getTipoEstado().getNivelVida();
+		float entrada = estado.pagarTrabajador();
+		float resultadoEsperado = ser.getTipoEstado().getNivelVida()/ entrada;
+		
+			if(necesidadVital > entrada) {
+				assertEquals(resultadoEsperado ,ser.calcularParteProporcional(ser));	
+		}
+		
 	}
 	
-
+	@Test
+	void testReducirEsperanzaVida() {
+		float esperanzaVida = ser.getEsperanzaVida();
+		float resultadoEsperado = esperanzaVida - ser.calcularParteProporcional(ser); 
+		
+		assertEquals(resultadoEsperado, ser.reducirEsperanzaVida(ser));
+		
+	}
+	
 }
