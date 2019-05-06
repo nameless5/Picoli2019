32d2d3d791c9f81e93a1e9f9346f5622a886ad03
hh3 <hh3@users.noreply.github.com>
2019-05-06 14:35:11 +0200

    main

---
+++ src/control/Main.java
@@ -0,0 +1,20 @@
+package control;
+
+import java.awt.EventQueue;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ParaUI frame = new ParaUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+}
